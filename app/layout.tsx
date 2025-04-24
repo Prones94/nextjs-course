8aaeb83668c2c84d1fd5629e86946e58269b057e
Ian <ronesp.85@gmail.com>
2025-04-24 11:53:37 -0700

    setup auth layouts, social auth form, next auth, OAuth with Google and provders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,59 +1,68 @@
-import Navbar from "@/components/navigation/navbar";
-import type { Metadata } from "next";
-import { ThemeProvider } from "next-themes";
-import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
+import type {Metadata} from "next";
+import {ThemeProvider} from "next-themes";
+import {Geist, Geist_Mono, Inter, Space_Grotesk} from "next/font/google";
+import {Toaster} from "@/components/ui/sonner"
 import "./globals.css";
+import {SessionProvider} from "next-auth/react";
+import {auth} from "@/auth";
+import React from "react";
 
 const inter = Inter({
-  variable: "--font-inter",
-  weight: "variable",
-  subsets: ["latin"],
+    variable: "--font-inter",
+    weight: "variable",
+    subsets: ["latin"],
 })
 
 const spaceGrotesk = Space_Grotesk({
-  variable: "--font-space-grotesk",
-  weight: "variable",
-  subsets: ["latin"]
+    variable: "--font-space-grotesk",
+    weight: "variable",
+    subsets: ["latin"]
 })
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description: "A better version of Stack Overflow.",
-  icons: {
-    icon: '/images/site-logo.svg'
-  }
+    title: "DevFlow",
+    description: "A better version of Stack Overflow.",
+    icons: {
+        icon: '/images/site-logo.svg'
+    }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+const RootLayout = async ({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>)  => {
+    const session = await auth()
+    return (
+        <html lang="en"
+              suppressHydrationWarning>
+        <SessionProvider session={session}>
+            <body
+                className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+            >
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+            <Toaster position="top-center"/>
+            </body>
+        </SessionProvider>
+        </html>
+    );
 }
+
+export default RootLayout
